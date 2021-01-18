5fbf7745dfefbc115a17937ba87766329a298888
Miguel <migueldp4@gmail.com>
2021-01-18 17:15:16 -0600

    Fix start screen, start culture page

--- src/components/NuestraCultura.js
+++ src/components/NuestraCultura.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import IconCulture from '../img/cultura.png';
 
 const NuestraCultura = () => {
   return (
     <section id='nuestra-cultura'>
-      
+      <div className='cultura-image-container'>
+        <img src={IconCulture}></img>
+      </div>
+      <div className='cultura-info'>
+        <div></div>
+        <div></div>
+      </div>
     </section>
   );
 }
