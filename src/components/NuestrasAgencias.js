615cf3ee27dbccadc78ca372bf5bc17fa9c21214
Miguel <migueldp4@gmail.com>
2021-01-19 15:37:43 -0600

    add nuestras agencias section

--- src/components/NuestrasAgencias.js
+++ src/components/NuestrasAgencias.js
@@ -1,9 +1,18 @@
 import React from 'react';
-
+import IconAgencias from '../img/agencias.png';
+import Map from '../img/map.png';
 const NuestrasAgencias = () => {
   return (
     <section id='nuestras-agencias'>
-      
+      <div id='nuestras-agencias-anchor'></div>
+      <div className='agencias-image-container'>
+        <img src={IconAgencias} />
+      </div>
+      <h3>Nuestras Agencias</h3>
+      <div className='small-orange-underline'></div>
+      <div className='agencias-container'>
+        <img src={Map}/>
+      </div>
     </section>
   );
 }
